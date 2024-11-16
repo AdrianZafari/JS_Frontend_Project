bbd2ec34e03e42ddbf5cc5a2cdae9958a5bd3e6a
AdrianZafari <adrian006@hotmail.com>
2024-11-16 13:46:12 +0100

    Fixed text for the medical centers in location section of Contact page

--- src/Components/Location.jsx
+++ src/Components/Location.jsx
@@ -24,9 +24,9 @@ function Location() {
                              />
 
 
-            <Location_item title={'Medical Center 1'}
-                             address={'4517 Washington Ave. Manchester, Kentucky 39495'}
-                             phoneNo={'(406) 555-0120'} 
+            <Location_item title={'Medical Center 2'}
+                             address={'2464 Royal Ln. Mesa, New Jersey 45463'}
+                             phoneNo={'(406) 544-0123'} 
                              week_opening={"9:00"}
                              week_closing={"22:00"}
                              weekend_opening={"9:00"}
